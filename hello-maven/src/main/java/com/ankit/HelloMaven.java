c67d85d5a428e8574a7b1734b3f6bcac21ae5943
Ankit <ankit@sap.com>
2020-02-08 15:15:21 +0530

    Adding logback to maven project

--- hello-maven/src/main/java/com/ankit/HelloMaven.java
+++ hello-maven/src/main/java/com/ankit/HelloMaven.java
@@ -1,7 +1,14 @@
 package com.ankit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HelloMaven {
+
+    private static final Logger log = LoggerFactory.getLogger(HelloMaven.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        log.info("Hello Info!");
+        log.debug("Hello Debug!");
     }
 }
