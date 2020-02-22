faf87e31f078aeb7a10cc30d0619fb47ba28f437
Ankit <ankit@sap.com>
2020-02-22 20:30:48 +0530

    model and model attributes

--- todo-list/src/main/java/com/ankit/controller/DemoController.java
+++ todo-list/src/main/java/com/ankit/controller/DemoController.java
@@ -1,9 +1,13 @@
 package com.ankit.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 public class DemoController {
 
@@ -13,9 +17,21 @@ public class DemoController {
         return "Hello";
     }
 
-    // prefix + name + suffix is returned from this method because of the ViewResolver
     @GetMapping("welcome")
-    public String welcome(){
+    public String welcome(Model model) {
+
+        model.addAttribute("user", "Ankit");
+        log.info("model= {}", model);
+
+        // prefix + name + suffix is returned from this method because of the ViewResolver
         return "welcome";
+    }
+
+    // executed before any other method, every time the controller receives a request
+    @ModelAttribute("welcomeMessage")
+    public String welcomeMessage() {
+        log.info("welcomeMessage() called");
+
+        return "Welcome to demo application";
     }
 }
