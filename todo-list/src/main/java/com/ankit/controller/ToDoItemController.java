4e713fade1e8832106cd9105dbf9bd107b2505c8
Ankit <ankit@sap.com>
2020-03-04 01:13:57 +0530

    view item jsp and controller added

--- todo-list/src/main/java/com/ankit/controller/ToDoItemController.java
+++ todo-list/src/main/java/com/ankit/controller/ToDoItemController.java
@@ -10,10 +10,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.management.Attribute;
+import javax.swing.text.View;
 import java.time.LocalDate;
 
 @Slf4j
@@ -42,19 +42,44 @@ public class ToDoItemController {
     }
 
     @GetMapping(Mappings.ADD_ITEM)
-    public String addEditItem(Model model) {
-        ToDoItem toAdd = new ToDoItem("", "", LocalDate.now());
-        model.addAttribute(AttributeNames.TODO_ITEM, toAdd);
+    public String addEditItem(@RequestParam(required = false, defaultValue = "-1") int id, Model model) {
+        log.info("editing id = {}", id);
+        ToDoItem toDoItem = toDoItemService.getItem(id);
+
+        if (toDoItem == null) {
+            toDoItem = new ToDoItem("", "", LocalDate.now());
+        }
+
+        model.addAttribute(AttributeNames.TODO_ITEM, toDoItem);
         return ViewNames.ADD_ITEM;
     }
 
     // to automatically get the ToDoItem parameter from the form, we need to add a @ModelAttribute annotation to the parameter
     // name of the parameter given in parentheses should match the name of the variable given in the form in jsp
     @PostMapping(Mappings.ADD_ITEM)
-    public String processItem(@ModelAttribute(AttributeNames.TODO_ITEM) ToDoItem toAdd) {
-        log.info("ToDoItem from form = {}", toAdd);
-        toDoItemService.addItem(toAdd);
+    public String processItem(@ModelAttribute(AttributeNames.TODO_ITEM) ToDoItem toDoItem) {
+        log.info("ToDoItem from form = {}", toDoItem);
+
+        if (toDoItem.getId() == 0) {
+            toDoItemService.addItem(toDoItem);
+        } else {
+            toDoItemService.updateItem(toDoItem);
+        }
+
         // after adding an item we want to redirect to a page showing all items
         return "redirect:/" + Mappings.ITEMS;
+    }
+
+    @GetMapping(Mappings.DELETE_ITEM)
+    public String deleteItem(@RequestParam int id) {
+        toDoItemService.removeItem(id);
+        return "redirect:/" + Mappings.ITEMS;
+    }
+
+    @GetMapping(Mappings.VIEW_ITEM)
+    public String viewItem(@RequestParam int id, Model model) {
+        ToDoItem toDoItem = toDoItemService.getItem(id);
+        model.addAttribute(AttributeNames.TODO_ITEM, toDoItem);
+        return ViewNames.VIEW_ITEM;
     }
 }
