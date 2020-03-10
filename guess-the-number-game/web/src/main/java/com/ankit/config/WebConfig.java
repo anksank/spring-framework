9a16e0fe166bf9ec97daf040891708812cf17666
Ankit <ankit@sap.com>
2020-03-10 12:18:51 +0530

    adding web mvc configuration and thymeleaf

---
+++ guess-the-number-game/web/src/main/java/com/ankit/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.ankit.config;
+
+import com.ankit.util.ViewNames;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName(ViewNames.HOME);
+    }
+}
---
+++ guess-the-number-game/web/src/main/java/com/ankit/util/ViewNames.java
@@ -0,0 +1,11 @@
+package com.ankit.util;
+
+
+public final class ViewNames {
+
+    // == constants ==
+    public static final String HOME = "home";
+
+    // == constructors ==
+    private ViewNames() { }
+}
