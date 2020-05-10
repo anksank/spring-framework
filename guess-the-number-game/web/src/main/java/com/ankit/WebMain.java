f0e712c16284855291461d21d0b46ba8c19021af
Ankit <ankit@sap.com>
2020-05-10 17:44:18 +0530

    Adding templates for all game views and controllers

--- guess-the-number-game/web/src/main/java/com/ankit/WebMain.java
+++ guess-the-number-game/web/src/main/java/com/ankit/WebMain.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebMain {
-
     public static void main(String[] args) {
         SpringApplication.run(WebMain.class, args);
     }
